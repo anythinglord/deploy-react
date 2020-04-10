ce88c11bf4cef648efdfa2d0be1bc60118f02e27
anythinglord <sjsalcedoa@unal.edu.co>
2020-04-10 11:24:54 -0500

    Updates

---
+++ precache-manifest.cd28be69ddf21e34058e31967f450f19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0ce131f8daa0c8228d2e1f4096fe666",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "3095b5696dce5ece0aa6",
+    "url": "/deploy-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "92a787576c694bb195f4",
+    "url": "/deploy-react/static/js/2.c2c73c81.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-react/static/js/2.c2c73c81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3095b5696dce5ece0aa6",
+    "url": "/deploy-react/static/js/main.c3942ae9.chunk.js"
+  },
+  {
+    "revision": "eaa17fc5625dd2952b20",
+    "url": "/deploy-react/static/js/runtime-main.9ff3a7b1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
